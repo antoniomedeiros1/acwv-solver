07ef93410ee0603405a4f0f0de0ff61dd34a582f
Antonio <antonio.medeiros@estudante.ufjf.br>
2021-06-09 21:35:09 -0300

    organizei a estrutura dos arquivos em pastas e inclui o CMake

---
+++ build/CMakeFiles/mdf.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mdf.dir/main.cpp.o"
+  "CMakeFiles/mdf.dir/main.cpp.o.d"
+  "CMakeFiles/mdf.dir/src/Matriz2d.cpp.o"
+  "CMakeFiles/mdf.dir/src/Matriz2d.cpp.o.d"
+  "mdf"
+  "mdf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mdf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mdf.dir/src/Matriz2d.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/mdf.dir/src/Matriz2d.cpp.o: \
+ /home/antonio/IC/mdf-onda-acustica/src/Matriz2d.cpp \
+ /usr/include/stdc-predef.h \
+ /home/antonio/IC/mdf-onda-acustica/src/../include/Matriz2d.h
